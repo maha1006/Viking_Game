8459f57aa1ff5323543fe4e94a84c3c0a9cc722a
unknown <maha1006@stud.hs-kl.de>
2021-05-06 14:40:20 +0200

    Erste Erfolge_Boat Controller

--- viking game/Assets/Scripts/BoatController.cs
+++ viking game/Assets/Scripts/BoatController.cs
@@ -5,25 +5,34 @@ using UnityEngine;
 public class BoatController : MonoBehaviour
 {
     private float moveInput;
+    private float turnInput;
 
     public float fwdSpeed;
+    public float revSpeed;
+    public float turnSpeed;
 
     public Rigidbody sphereRB;
 
 
     void Start()
     {
-        
+        sphereRB.transform.parent = null;
+
     }
 
     void Update()
     {
 
         moveInput = Input.GetAxisRaw("Vertical");
-        moveInput *= fwdSpeed;
+        turnInput = Input.GetAxisRaw("Horizontal");
+
+        moveInput *= moveInput > 0 ? fwdSpeed : revSpeed;
 
 
         transform.position = sphereRB.transform.position;
+
+        float newRotation = turnInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+        transform.Rotate(0, newRotation, 0, Space.World);
     }
 
     private void FixedUpdate()
