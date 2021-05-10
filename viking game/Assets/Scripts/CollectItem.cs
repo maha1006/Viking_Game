c3dabcf806924c967507eeb3a12b86b8ea56e6fb
unknown <maha1006@stud.hs-kl.de>
2021-05-10 12:26:18 +0200

    Score System

---
+++ viking game/Assets/Scripts/CollectItem.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItem : MonoBehaviour
+{
+    public AudioSource collectSound;
+    void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        ScoringSystem.theScore += 1;
+        Destroy(gameObject);
+    }
+
+
+}
+
---
+++ viking game/Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour
+{
+    public GameObject Wood_Score;
+    public static int theScore;
+
+    void Update()
+    {
+        Wood_Score.GetComponent<Text>().text = "Wood: " + theScore;
+    }
+
+}
