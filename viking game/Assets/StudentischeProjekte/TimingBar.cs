21d1602c18421900b75fcd7d12c37d3e887d4d7a
unknown <maha1006@stud.hs-kl.de>
2021-05-18 18:00:22 +0200

    Olli der Retter in der Not

---
+++ viking game/Assets/StudentischeProjekte/TimingBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimingBar : MonoBehaviour
+{
+    public Transform Zeiger;
+    public Transform PosA;
+    public Transform PosB;
+    float initialOffset;
+    float currentPosition;
+
+    void Start()
+    {
+        initialOffset = Random.Range(0f, 1f);
+        Zeiger.localPosition = Vector3.Lerp(PosA.localPosition, PosB.localPosition, initialOffset);
+    }
+
+    void Update()
+    {
+        currentPosition = Mathf.PingPong(Time.time + initialOffset, 1f);
+        Zeiger.localPosition = Vector3.Lerp(PosA.localPosition, PosB.localPosition, currentPosition);
+    }
+}
