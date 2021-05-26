eea1c182738fbee479c1b84415baa205ff720e11
unknown <maha1006@stud.hs-kl.de>
2021-05-26 10:58:03 +0200

    Fish Flock (nicht fertig)

---
+++ viking game/Assets/Scripts/Flock.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+
+    [Header("Spawn Setup")]
+    [SerializeField] private FlockUnit flockUnitPrefab;
+    [SerializeField] private int flockSize;
+    [SerializeField] private Vector3 spawnBounds;
+
+    [Header("Speed Setup")]
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+
+    [Header("Detection Distances")]
+    [Range(0, 10)]
+    [SerializeField] private float _cohesionDistance;
+
+    public float cohesionDistance { get { return cohesionDistance; } }
+    public FlockUnit[] allUnits { get; set; }
+
+    private void Start()
+    {
+        GenerateUnits();
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < allUnits.Length; i++)
+        {
+            allUnits[i].MoveUnit();
+        }
+    }
+
+    private void GenerateUnits()
+    {
+        allUnits = new FlockUnit[flockSize];
+        for (int i = 0; i < flockSize; i++)
+        {
+            var randomVector = UnityEngine.Random.insideUnitSphere;
+            randomVector = new Vector3(randomVector.x * spawnBounds.x, randomVector.y * spawnBounds.y, randomVector.z * spawnBounds.z);
+            var spawnPosition = transform.position + randomVector;
+            var rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0, 360), 0);
+            allUnits[i] = Instantiate(flockUnitPrefab, spawnPosition, rotation);
+            allUnits[i].AssignFlock(this);
+        }
+    }
+}
