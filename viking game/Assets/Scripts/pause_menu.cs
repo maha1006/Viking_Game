60ec1120ac81a015d9fc59911b64710d5b6adf20
unknown <maha1006@stud.hs-kl.de>
2021-06-28 18:15:57 +0200

    Revert "pause menu new"
    This reverts commit d8ac553690ff80ee7945ee767735b630b0cc1969.

--- viking game/Assets/Scripts/pause_menu.cs
+++ viking game/Assets/Scripts/pause_menu.cs
@@ -10,7 +10,7 @@ public class pause_menu : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.Escape))
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
             if(GameIsPaused)
             {
