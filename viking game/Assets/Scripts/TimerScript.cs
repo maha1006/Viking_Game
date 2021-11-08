def4834b5a13733ed5590867bf87f9f2d68047c7
erpl1001 <81622612+erpl1001@users.noreply.github.com>
2021-11-08 13:17:02 +0100

    dash final

--- viking game/Assets/Scripts/TimerScript.cs
+++ viking game/Assets/Scripts/TimerScript.cs
@@ -9,37 +9,83 @@ public class TimerScript : MonoBehaviour
     public float maxTime = 5f;
     float minTime = 1f;
     public float timeLeft;
+    float lastValue;
     //public GameObject timesUpText;
-    
+
+    public bool decreasing;
+    public bool increasing;
+
     void Start()
     {
         timerBar = GetComponent<Image>();
         timeLeft = maxTime;
+        lastValue = timerBar.fillAmount;
     }
 
     
 
     void Update()
     {
-        
-         if (Input.GetButton("Fire3"))
-         {
+        if (timerBar.fillAmount < lastValue)
+        {
+            //Debug.Log("decreased");
+            decreasing = true;
+            increasing = false;
+           lastValue = timerBar.fillAmount;
+        }
+
+        else if (timerBar.fillAmount > lastValue)
+        {
+            //Debug.Log("increased");
+            decreasing = false;
+            increasing = true;
+           lastValue = timerBar.fillAmount;
+            
+        }
+
+        if (timerBar.fillAmount > 0.9f)
+        {
+            decreasing = true;
+            increasing = false;
+        }
+
+        if (timerBar.fillAmount < 0.05f)
+        {
+            decreasing = false;
+            increasing =true;
+        }
+
+        if (decreasing)
+        {
+            //Debug.Log("chillig");
+
+            if (Input.GetButton("Fire3"))
+            {
                 if (timeLeft > 0)
                 {
                     timeLeft -= Time.deltaTime;
                     timerBar.fillAmount = timeLeft / maxTime;
                     //print("space key was pressed");
                 }
-         }
-        
-
-        else
-        {
-            if (timeLeft < maxTime)
-            {
-                timeLeft += Time.deltaTime / 5;
-                timerBar.fillAmount = timeLeft / maxTime;
             }
+
+
+
+
+            else 
+            {
+                if (timeLeft < maxTime)
+                {
+                    timeLeft += Time.deltaTime / 5;
+                    timerBar.fillAmount = timeLeft / maxTime;
+                }
+            }
+        }
+        
+        if (increasing)
+        {
+            timeLeft += Time.deltaTime / 5;
+            timerBar.fillAmount = timeLeft / maxTime;
         }
         
     }
